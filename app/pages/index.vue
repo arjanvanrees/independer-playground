356b55ac277c8e6bab4c219a2be902ddaad34cce
Arjan van Rees <arjanvanrees@gmail.com>
2026-07-13 09:10:24 +0200

    Install independer-design-system

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-3xl font-serif font-bold text-purple-dark">Welcome to the Independer Playground</h1>
+
+    <div class="flex gap-2 mt-4">
+        <Icon name="eva:info-outline" class="size-6" />
+        Change this file to get started.
+    </div>
   </div>
 </template>
